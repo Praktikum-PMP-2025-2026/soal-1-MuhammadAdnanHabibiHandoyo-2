d1e3615eafa546e93258815001abc5e1a74b0e22
Adnhh <habibi7j@gmail.com>
2026-05-11 08:48:12 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -67,7 +67,7 @@ int main(){
     if(ada == 0){
         printf("ISOLATED NONE\n");
     }else{
-        printf("ISOLATED");
+        printf("ISOLATED ");
         for(int i=0; i<N; i++){
             if(idxIsolated[i] == 1){
                 printf("%d", i);
